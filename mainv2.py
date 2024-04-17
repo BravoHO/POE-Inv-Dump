77b04681b6dfa824ed5f7b4942f16e98c2883b30
BravoHO <57259381+BravoHO@users.noreply.github.com>
2024-04-18 02:37:49 +0800

    Update mainv2.py
    Added some new functions

--- mainv2.py
+++ mainv2.py
@@ -3,6 +3,7 @@ import pyautogui as pg
 import time
 import random
 
+
 def toggle_cell(row, col):
     global start_row, start_col, end_row, end_col
 
@@ -22,6 +23,7 @@ def toggle_cell(row, col):
             grid[row][col] = 1
             canvas.itemconfig(cells[row][col], fill='black')
 
+
 def select_area():
     min_row = min(start_row, end_row)
     max_row = max(start_row, end_row)
@@ -34,9 +36,11 @@ def select_area():
                 grid[i][j] = 1
                 canvas.itemconfig(cells[i][j], fill='black')
 
+
 def on_shift_press(event):
     global shift_pressed
     shift_pressed = True
+
 
 def on_shift_release(event):
     global shift_pressed, start_row, start_col, end_row, end_col
@@ -45,6 +49,7 @@ def on_shift_release(event):
     start_col = None
     end_row = None
     end_col = None
+
 
 def create_grid(rows, cols, cell_size):
     for i in range(rows):
@@ -57,6 +62,7 @@ def create_grid(rows, cols, cell_size):
             cells[i][j] = cell
             canvas.tag_bind(cell, '<Button-1>', lambda event, r=i, c=j: toggle_cell(r, c))
 
+
 def depot():
     y = 588
     x = 1272
@@ -64,18 +70,29 @@ def depot():
     for yy in range(rows):
         for xx in range(cols):
             if grid[yy][xx] == 1:
-                clicker = random.randint(52,52)
-                pg.click((x+26+(xx*clicker)),(y+26+(yy*clicker)))
-                sleeper = random.randint(15,25)/100
-                time.sleep(sleeper)
+                clicker = random.randint(50, 55)
+                pg.moveTo((x + 26 + (xx * clicker)), (y + 26 + (yy * clicker)))
+                pg.click((x + 26 + (xx * clicker)), (y + 26 + (yy * clicker)))
+                sleeper = random.randint(5, 10) / 100
+                # time.sleep(sleeper)
     pg.keyUp('ctrl')
 
     pass
+
+
+def dump():
+    pg.keyDown('shift')
+    depot()
+    pg.keyUp('shift')
+
+
 def clear():
     for i in range(rows):
         for l in range(cols):
             grid[i][l] = 0
             canvas.itemconfig(cells[i][l], fill='white')
+
+
 rows = 5
 cols = 12
 cell_size = 53
@@ -83,7 +100,7 @@ cell_size = 53
 root = tk.Tk()
 root.title("ID")
 root.geometry("+1260+270")
-canvas = tk.Canvas(root, width=cols*cell_size, height=rows*cell_size, bg='white')
+canvas = tk.Canvas(root, width=cols * cell_size, height=rows * cell_size, bg='white')
 canvas.config(bg="#000000")
 canvas.pack()
 
@@ -102,7 +119,9 @@ root.bind('<Shift_L>', on_shift_press)
 root.bind('<KeyRelease-Shift_L>', on_shift_release)
 
 ok_button = tk.Button(root, text="OK", command=depot)
-ok_button.pack(side="left",anchor="e",expand=True)
-clear_button = tk.Button(root,text="CLEAR", command=clear)
-clear_button.pack(side="left",anchor="w",expand=True)
+ok_button.pack(side="left", anchor="n", expand=True)
+clear_button = tk.Button(root, text="CLEAR", command=clear)
+clear_button.pack(side="left", anchor="n", expand=True)
+dump_onetab_button = tk.Button(root, text="DUMP", command=dump)
+dump_onetab_button.pack(side="left", anchor="n", expand=True)
 root.mainloop()
